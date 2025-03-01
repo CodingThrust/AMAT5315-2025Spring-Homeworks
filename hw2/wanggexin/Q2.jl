3d148cccf7c0f0e923f8feec2565d588d8cde876
WangGexin <gwang827@connect.hkust-gz.edu.cn>
2025-03-01 19:44:29 +0800

    update wanggexin's HW2

--- hw2/wanggexin/Q2.jl
+++ hw2/wanggexin/Q2.jl
@@ -1,5 +1,39 @@
 
 
+# # 定义并集操作 ⊕
+# function ⊕(s::Set{Int}, t::Set{Int})
+#     return s ∪ t
+# end
+
+# # 定义乘积操作 ⊙
+# function ⊙(s::Set{Int}, t::Set{Int})
+#     return Set(x * y for x in s for y in t)
+# end
+
+# # 加法单位元 0
+# const zero_set = Set{Int}()
+
+# # 乘法单位元 1
+# const one_set = Set([1])
+
+# # 测试用例
+# println("⊕({2}, {5, 4}) = ", ⊕(Set([2]), Set([5, 4])))
+# println("⊕({2}, {}) = ", ⊕(Set([2]), zero_set))
+# println("⊙({2}, {5, 4}) = ", ⊙(Set([2]), Set([5, 4])))
+# println("⊙({2}, {}) = ", ⊙(Set([2]), zero_set))
+# println("⊙({2}, {1}) = ", ⊙(Set([2]), one_set))
+
+# # ⊕({2}, {5, 4}) = Set([5, 4, 2])
+# # ⊕({2}, {}) = Set([2])
+# # ⊙({2}, {5, 4}) = Set([10, 8])
+# # ⊙({2}, {}) = Set{Int64}()
+# # ⊙({2}, {1}) = Set([2])
+# ====== history ======
+
+
+
+using Test
+
 # 定义并集操作 ⊕
 function ⊕(s::Set{Int}, t::Set{Int})
     return s ∪ t
@@ -17,14 +51,10 @@ const zero_set = Set{Int}()
 const one_set = Set([1])
 
 # 测试用例
-println("⊕({2}, {5, 4}) = ", ⊕(Set([2]), Set([5, 4])))
-println("⊕({2}, {}) = ", ⊕(Set([2]), zero_set))
-println("⊙({2}, {5, 4}) = ", ⊙(Set([2]), Set([5, 4])))
-println("⊙({2}, {}) = ", ⊙(Set([2]), zero_set))
-println("⊙({2}, {1}) = ", ⊙(Set([2]), one_set))
+@test ⊕(Set([2]), Set([5, 4])) == Set([2, 4, 5])
+@test ⊕(Set([2]), zero_set) == Set([2])
+@test ⊙(Set([2]), Set([5, 4])) == Set([10, 8])
+@test ⊙(Set([2]), zero_set) == Set{Int}()
+@test ⊙(Set([2]), one_set) == Set([2])
 
-# ⊕({2}, {5, 4}) = Set([5, 4, 2])
-# ⊕({2}, {}) = Set([2])
-# ⊙({2}, {5, 4}) = Set([10, 8])
-# ⊙({2}, {}) = Set{Int64}()
-# ⊙({2}, {1}) = Set([2])
+println("All tests passed!")
