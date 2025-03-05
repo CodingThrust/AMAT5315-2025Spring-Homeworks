230cb9560c690398052eebe0eab1d40bb77c4ef4
Zhaohui Zhi <2276780832@qq.com>
2025-03-05 21:08:37 +0800

    edit

--- hw4/zhaohui_zhi/main.jl
+++ hw4/zhaohui_zhi/main.jl
@@ -40,7 +40,7 @@ C = 3  # example size
 Mat = diagm(1 => C.*ones(n-1)) +  diagm(-1 => C.*ones(n-1)) + diagm(0 => -2C.*ones(n))
 Mat[1, 1] = -C
 Mat[end, end] = -C
-vals, vecs = eigen(Mat)
-plot(vals)
+M = -diagm(vcat(repeat([1, 2],10),1)) 
+vals, vecs = eigen(Mat*inv(M))
 
-@show vals
+@show sqrt.(vals)
