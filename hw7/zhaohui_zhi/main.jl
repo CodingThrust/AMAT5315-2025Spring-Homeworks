928529cf7e45e77162b18b443200a65f1d647933
Zhaohui Zhi <2276780832@qq.com>
2025-04-02 20:46:59 +0800

    initial hw

---
+++ hw7/zhaohui_zhi/main.jl
@@ -0,0 +1,19 @@
+using Graphs, ProblemReductions
+
+function fullerene()  # construct the fullerene graph in 3D space
+    th = (1+sqrt(5))/2
+    res = NTuple{3,Float64}[]
+    for (x, y, z) in ((0.0, 1.0, 3th), (1.0, 2 + th, 2th), (th, 2.0, 2th + 1.0))
+        for (a, b, c) in ((x,y,z), (y,z,x), (z,x,y))
+            for loc in ((a,b,c), (a,b,-c), (a,-b,c), (a,-b,-c), (-a,b,c), (-a,b,-c), (-a,-b,c), (-a,-b,-c))
+                if loc ∉ res
+                    push!(res, loc)
+                end
+            end
+        end
+    end
+    return res
+end
+
+fullerene_graph = UnitDiskGraph(fullerene(), sqrt(5))
+gplot(fullerene_graph, layout=spring_layout)
