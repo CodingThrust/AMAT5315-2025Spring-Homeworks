d83a8c67db95abfe2fab80152e58b4f7156e804e
Yui <2946723935@qq.com>
2025-04-25 10:17:26 +0800

    I have expanded sizes to at least 18

--- hw7/kaiwenjin/hw7-p2.jl
+++ hw7/kaiwenjin/hw7-p2.jl
@@ -5,17 +5,16 @@ model = ["triangles","squares","diamonds"]
 # 2.1
 ########################################################
 Random.seed!(42)
-N0 = [12,12,13]
+N0 = [18,18,19]
 T = collect(0.1:0.1:2.0)
-energy_gap_T = zeros(length(T),length(model))
+energy_gap = zeros(length(T),length(model))
 
 for n = 1:3
     N = N0[n]
     for t = 1:length(T)
-        @show n,t
         A = generate_topology_matrix(N,model[n])
         H = generate_hamiltonian(A,T[t])
-        E,_ = eigsolve(H,normalize(rand(2^N)),10,:SR,KrylovKit.Lanczos())
+        E,_ = eigsolve(H,normalize(normalize!(rand(2^N))), 10,:SR,KrylovKit.Lanczos())
         e2 = E[2]   
         flag = false
         for i in 2:10
@@ -32,16 +31,16 @@ for n = 1:3
     end
 end
 
-plot(T,energy_gap_T[:,1],label="triangles,sites=12",xlabel="Temperature",ylabel="Energy gap",title="Energy gap vs Temperature")
-plot!(T,energy_gap_T[:,2],label="squares,sites=12")
-plot!(T,energy_gap_T[:,3],label="diamonds,sites=13")
+plot(T,energy_gap[:,1],label="triangles,sites=$(N0[1])",xlabel="Temperature",ylabel="Energy gap",title="Energy gap vs Temperature")
+plot!(T,energy_gap[:,2],label="squares,sites=$(N0[2])")
+plot!(T,energy_gap[:,3],label="diamonds,sites=$(N0[3])")
 
 
 ########################################################
 # 2.2
 ########################################################
 Random.seed!(42)
-N1 = [collect(6:2:16),collect(6:2:16),collect(7:3:16)]
+N1 = [collect(6:2:18),collect(6:2:18),collect(7:3:19)]
 T1 = 0.1
 energy_gap_N = [zeros(length(N1[n])) for n = 1:3]
 for n = 1:3
