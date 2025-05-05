c058bc74bab1c0ea0e2203401fd5e49241247b14
Zhaohui Zhi <2276780832@qq.com>
2025-05-05 16:46:01 +0800

    commit

--- hw8/main.jl
+++ hw8/main.jl
@@ -1,7 +1,10 @@
+using Pkg
+Pkg.activate("./hw8/zhaohui_zhi")
+Pkg.instantiate()
 using ProblemReductions, GenericTensorNetworks
 using SpinDynamics
 using Graphs
-
+using Plots
 circuit = @circuit begin
     s = x ⊻ y  
     c = x ∧ y
@@ -27,7 +30,7 @@ h= Float64.(SG.h)
 
 
 initial_scale = 0.1 
-initial_x = randn(nv(graph)) .* initial_scale
+initial_x = [1.0, 0.0, 0.0, 1.0, 1.0] # Initial state for the x and y variables
 initial_p = randn(nv(graph)) .* initial_scale
 initial_state = SimulatedBifurcationState(initial_x, initial_p)
 
@@ -37,22 +40,21 @@ bifurcation_model = SimulatedBifurcation{:aSB}(graph, J, c0=0.04902903378454601)
 
 nsteps = 1000
 dt = 0.01
-state, checkpoints = simulate_bifurcation!(initial_state, bifurcation_model; nsteps=nsteps, dt=dt)
+state, checkpoints = simulate_bifurcation!(initial_state, bifurcation_model; nsteps=nsteps, dt=dt,  checkpoint_steps=10)
 
 # times = getfield.(checkpoints, :time)
 # potential_energies = getfield.(checkpoints, :potential_energy)
 # kinetic_energies = getfield.(checkpoints, :kinetic_energy)
 # total_energies = potential_energies .+ kinetic_energies
 
-resolution=100
-xmax =  1.0
-x_range = range(-xmax, xmax, length=resolution)
-y_range = range(-xmax, xmax, length=resolution)
-
 # final_energy = SpinDynamics.potential_energy.(Ref(bifurcation_model), [[x, y] for x in x_range, y in y_range])
 
 xs = [s.state.x[1] for s in checkpoints]
 ys = [s.state.x[2] for s in checkpoints]
+
+@show xs[end], ys[end]
+
+#The input x=1, y=0
 
 # ground_state = read_config(solve(SG, SingleConfigMin())[])
 # @info "Ground state configuration: $(ground_state)"
